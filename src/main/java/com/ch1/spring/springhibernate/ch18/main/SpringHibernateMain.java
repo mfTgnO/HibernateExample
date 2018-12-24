7a3187987ac3ddc1465a12d08c997ede16138a97
wl <wl@gmail.com>
2018-12-24 18:58:38 +0800

    ch07:Spring Bean Life Cycle

--- src/main/java/com/ch1/spring/springhibernate/ch18/main/SpringHibernateMain.java
+++ src/main/java/com/ch1/spring/springhibernate/ch18/main/SpringHibernateMain.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public class SpringHibernateMain {
     public static void main(String[] args) {
+//        test1();
+        test2();
+    }
+
+    private static void test1() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         PersonDAO personDAO = context.getBean(PersonDAO.class);
 
@@ -24,5 +29,14 @@ public class SpringHibernateMain {
 
         //close resources
         context.close();
+    }
+
+    private static void test2() {
+        String str = "有志者，事竟成，破釜沉舟，百二秦关终属楚；苦心人，天不负，卧薪尝胆，三千越甲可吞吴。——蒲松龄";
+        System.out.println(str);
+
+        String result = str.substring(0, 10);
+        System.out.println(result);
+
     }
 }
